da85db88ecc494e3499d1cda9afc67fb87da6d13
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-11-13 02:15:32 +0000

    Bootstrap + All Tasks Done

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,23 +1,31 @@
 // DB Connection & Promise-based Query
-const {query} = require("../db/promise-mysql");
-const {cp} = require("../db/connection");
+const { query } = require("../db/promise-mysql");
+const { cp } = require("../db/connection");
 const mysql = require("mysql");
 
 
 // GET '/api/songs'
-exports.getSongs = (req,res)=>{
-// Send all Albums
-    query(cp,`SELECT * from songs`)
+exports.getSongs = (req, res) => {
+    // Send all Songs - with Left JOIN - to include NULL
+    query(cp, `SELECT songs.name song_name, albums.name album_name from songs LEFT JOIN albums ON songs.album_id = albums.album_id;`)
         .then(results => res.send(results))
         .catch(error => res.send(error));
 }
 
-// GET '/api/album-songs'
-exports.getAlbumSongs = (req,res)=>{
-res.send('get album songs!');
+// GET '/api/songs/album-songs'
+exports.getAlbumSongs = (req, res) => {
+    query(cp, `SELECT * from songs_on_albums`)
+        .then(results => res.send(results))
+        .catch(error => res.send(error));
 }
 
 // POST '/api/songs'
-exports.postSongs = (req,res)=>{
- 
+exports.postSongs = (req, res) => {
+    let songName = mysql.escape(req.body.name);
+    let albumID = req.body.album_id == 0 ? "NULL" : mysql.escape(req.body.album_id);
+
+    let insertSongQuery = `INSERT INTO songs(name,album_id) VALUES (${songName},${albumID})`;
+    query(cp, insertSongQuery)
+        .then(results => console.log(`Success: ${insertSongQuery}`))
+        .catch(error => { console.log(error); });
 }
--- public/index.js
+++ public/index.js
@@ -1,89 +1,94 @@
 // Get albums and place in select list in New Song form
-const populateAlbumSelect=() =>{
+const populateAlbumSelect = () => {
     axios.get('/api/albums')
-    .then(results=>{
+        .then(results => {
 
-        const selectList = document.getElementById('album-select');
+            const selectList = document.getElementById('album-select');
 
-        selectList.innerHTML = "";
+            selectList.innerHTML = "";
 
-        let optionNone = document.createElement('option');
+            let optionNone = document.createElement('option');
 
-           optionNone.innerHTML = 'none';
-           optionNone.value=0;
+            optionNone.innerHTML = 'none';
+            optionNone.value = 0;
 
-           selectList.appendChild(optionNone);
+            selectList.appendChild(optionNone);
 
-        results.data.forEach(album=>{
+            results.data.forEach(album => {
 
-           let option = document.createElement('option');
+                let option = document.createElement('option');
 
-           option.innerHTML = album.name;
-           option.value=album.album_id;
+                option.innerHTML = album.name;
+                option.value = album.album_id;
 
-           selectList.appendChild(option);
+                selectList.appendChild(option);
 
-        });
+            });
 
-    })
-    .catch(error=>{console.log(error)});
+        })
+        .catch(error => { console.log(error) });
 }
 
 populateAlbumSelect();
 
 // Handle form submit for New Album
-const albumSubmit = (event)=>{
+const albumSubmit = (event) => {
     event.preventDefault();
 
+    populateAlbumSelect();
     const newAlbum = {
-        name:document.getElementById('album-name').value,
-        genre:document.getElementById('album-genre').value,
+        name: document.getElementById('album-name').value,
+        genre: document.getElementById('album-genre').value,
     }
 
     axios.post('/api/albums', newAlbum)
-    .then(result=>{
-        console.log(result.data);
-        populateAlbumSelect();
-    })
-    .catch(error=>console.log(error));
+        .then(result => {
+            console.log(result.data);
+            populateAlbumSelect();
+        })
+        .catch(error => console.log(error));
 }
 
 let formAddAlbum = document.getElementById('add-album');
 formAddAlbum.addEventListener('submit', albumSubmit);
 
 // Handle form submit for new Song
-const songSubmit = (event)=>{
+const songSubmit = (event) => {
     event.preventDefault();
 
+    document.getElementById("all-songs").innerHTML = "";
+    populateAllSongs();
+
     const newSong = {
-        name:document.getElementById('song-name').value,
-        album_id:document.getElementById('album-select').value
+        name: document.getElementById('song-name').value,
+        album_id: document.getElementById('album-select').value
     }
 
     axios.post('/api/songs', newSong)
-    .then(result=>{
-        console.log(result.data);
-    })
-    .catch(error=>console.log(error));
+        .then(result => {
+            console.log(result.data);
+        })
+        .catch(error => console.log(error));
 }
 
 let formAddSong = document.getElementById('add-song');
 formAddSong.addEventListener('submit', songSubmit);
 
-let populateAllSongs = ()=>{
- axios.get('/api/songs')
- .then(songs=>{
-    let songList = document.getElementById("all-songs");
-    
-    // Append all songs as <li>s
-    songs.data.forEach(song => {
-        let songListItemDOM = document.createElement('li');
-        songListItemDOM.innerHTML = song.song_name;
-        songList.appendChild(songListItemDOM);
-    })
-    
-    
- })
- .catch(error=>{console.log(error)});
+let populateAllSongs = () => {
+    axios.get('/api/songs')
+        .then(songs => {
+            let songList = document.getElementById("all-songs");
+
+            // Append all songs as <li>s
+            songs.data.forEach(song => {
+                let songListItemDOM = document.createElement('li');
+                songListItemDOM.className = "list-group-item";
+                songListItemDOM.innerHTML = `<h5>${song.song_name}</h5> ${song.album_name ? `<p><small>from</small><h6>${song.album_name}</h6>` : ''}</p>`;
+                songList.appendChild(songListItemDOM);
+            })
+
+
+        })
+        .catch(error => { console.log(error) });
 }
 populateAllSongs(); // call the above function
